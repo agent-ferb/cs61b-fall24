d0a73a7610758a75532449c55675bec601c6548b
agenttasogare <shimerryyuu88@berkeley.edu>
2024-10-30 08:14:51 -0700

    Finished Lab 8

--- lab08/src/hashmap/MyHashMap.java
+++ lab08/src/hashmap/MyHashMap.java
@@ -1,6 +1,9 @@
 package hashmap;
 
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Set;
 
 /**
  *  A hash table-backed Map implementation.
@@ -27,11 +30,21 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
     /* Instance Variables */
     private Collection<Node>[] buckets;
     // You should probably define some more!
+    private int size;
+    private double loadFactor;
+    private int initialCapacity;
+
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final double DEFAULT_LOAD_FACTOR = 0.75;
 
     /** Constructors */
-    public MyHashMap() { }
+    public MyHashMap() {
+        this(DEFAULT_CAPACITY, DEFAULT_LOAD_FACTOR);
+    }
 
-    public MyHashMap(int initialCapacity) { }
+    public MyHashMap(int initialCapacity) {
+        this(initialCapacity, DEFAULT_LOAD_FACTOR);
+    }
 
     /**
      * MyHashMap constructor that creates a backing array of initialCapacity.
@@ -40,7 +53,21 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * @param initialCapacity initial size of backing array
      * @param loadFactor maximum load factor
      */
-    public MyHashMap(int initialCapacity, double loadFactor) { }
+    public MyHashMap(int initialCapacity, double loadFactor) {
+        if (initialCapacity <= 0) {
+            throw new IllegalArgumentException("Initial capacity must be greater than 0.");
+        }
+        if (loadFactor <= 0) {
+            throw new IllegalArgumentException("Load factor must be greater than 0.");
+        }
+        this.initialCapacity = initialCapacity;
+        this.loadFactor = loadFactor;
+        this.buckets = new Collection[initialCapacity];
+        for (int i = 0; i < initialCapacity; i++) {
+            buckets[i] = createBucket();
+        }
+        this.size = 0;
+    }
 
     /**
      * Returns a data structure to be a hash table bucket
@@ -64,10 +91,152 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      */
     protected Collection<Node> createBucket() {
         // TODO: Fill in this method.
-        return null;
+        return new LinkedList<>();
     }
 
     // TODO: Implement the methods of the Map61B Interface below
     // Your code won't compile until you do so!
+    private int getBucketIndex(K key) {
+        return Math.abs(key.hashCode() % buckets.length);
+    }
+    /**
+     * Associates the specified value with the specified key in this map.
+     * If the map already contains the specified key, replaces the key's mapping
+     * with the value specified.
+     *
+     * @param key
+     * @param value
+     */
+    @Override
+    public void put(K key, V value) {
+        if (key == null) {
+            throw new NullPointerException("Key cannot be null.");
+        }
+        int index = getBucketIndex(key);
+        Collection<Node> bucket = buckets[index];
+
+        for (Node node : bucket) {
+            if (node.key.equals(key)) {
+                node.value = value;
+                return;
+            }
+        }
+        bucket.add(new Node(key, value));
+        size++;
+
+        if ((double) size / buckets.length > loadFactor) {
+            resize();
+        }
+    }
+
+    private void resize() {
+        Collection<Node>[] oldBuckets = buckets;
+        int newCapacity = oldBuckets.length * 2; // Geometrically resize
+        buckets = new Collection[newCapacity];
+        for (int i = 0; i < newCapacity; i++) {
+            buckets[i] = createBucket();
+        }
+        size = 0; // Reset size and reinsert elements
+
+        for (Collection<Node> bucket : oldBuckets) {
+            for (Node node : bucket) {
+                put(node.key, node.value); // Reinsert elements into new buckets
+            }
+        }
+    }
+
+    /**
+     * Returns the value to which the specified key is mapped, or null if this
+     * map contains no mapping for the key.
+     *
+     * @param key
+     */
+    @Override
+    public V get(K key) {
+        if (key == null) {
+            throw new NullPointerException("Key cannot be null.");
+        }
+        int index = getBucketIndex(key);
+        Collection<Node> bucket = buckets[index];
+
+        for (Node node : bucket) {
+            if (node.key.equals(key)) {
+                return node.value;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Returns whether this map contains a mapping for the specified key.
+     *
+     * @param key
+     */
+    @Override
+    public boolean containsKey(K key) {
+        if (key == null) {
+            throw new NullPointerException("Key cannot be null.");
+        }
+        int index = getBucketIndex(key);
+        Collection<Node> bucket = buckets[index];
+
+        for (Node node : bucket) {
+            if (node.key.equals(key)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Returns the number of key-value mappings in this map.
+     */
+    @Override
+    public int size() {
+        return size;
+    }
+
+    /**
+     * Removes every mapping from this map.
+     */
+    @Override
+    public void clear() {
+        for (int i = 0; i < buckets.length; i++) {
+            buckets[i].clear();
+        }
+        size = 0;
+    }
+
+    /**
+     * Returns a Set view of the keys contained in this map. Not required for this lab.
+     * If you don't implement this, throw an UnsupportedOperationException.
+     */
+    @Override
+    public Set<K> keySet() {
+        throw new UnsupportedOperationException("N/A");
+    }
+
+    /**
+     * Removes the mapping for the specified key from this map if present,
+     * or null if there is no such mapping.
+     * Not required for this lab. If you don't implement this, throw an
+     * UnsupportedOperationException.
+     *
+     * @param key
+     */
+    @Override
+    public V remove(K key) {
+        throw new UnsupportedOperationException("N/A");
+    }
+
+    /**
+     * Returns an iterator over elements of type {@code T}.
+     *
+     * @return an Iterator.
+     */
+    @Override
+    public Iterator<K> iterator() {
+        throw new UnsupportedOperationException("N/A");
+    }
 
 }
