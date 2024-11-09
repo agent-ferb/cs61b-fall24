5bb7b0cb22fe00e8039d9a89aeeb87c662526e44
agenttasogare <shimerryyuu88@berkeley.edu>
2024-11-08 22:52:47 -0800

    Finished Proj 2B

--- proj2b/src/main/AutograderBuddy.java
+++ proj2b/src/main/AutograderBuddy.java
@@ -1,6 +1,7 @@
 package main;
 
 import browser.NgordnetQueryHandler;
+import ngrams.NGramMap;
 
 
 public class AutograderBuddy {
@@ -9,11 +10,13 @@ public class AutograderBuddy {
      */
     private static final String WORDS_FILE = "data/ngrams/top_49887_words.csv";
 
-    public static NgordnetQueryHandler getHyponymsHandler() {
+    public static NgordnetQueryHandler getHyponymsHandler(String[] args) {
         String synsetsFile = Main.SMALL_SYNSET_FILE;
         String hyponymsFile = Main.SMALL_HYPONYM_FILE;
-        String wordsFile = WORDS_FILE; // Main.java uses this as the words file
+        String wordsFile = WORDS_FILE;
+        String countsFile = Main.TOTAL_COUNTS_FILE;
 
+        NGramMap ngm = new NGramMap(wordsFile, countsFile);
         WordNet wn = new WordNet(synsetsFile, hyponymsFile, wordsFile);
         return new HyponymsHandler(wn);
     }
